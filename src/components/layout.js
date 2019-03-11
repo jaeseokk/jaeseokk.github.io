749d9e49e6e116705e40a36e87e838bef81da47c
jaeseok-kang <kang89kr@gmail.com>
2019-03-12 00:35:22 +0900

    Migrate to gatsby.js

---
+++ src/components/layout.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import { rhythm, scale } from '../utils/typography'
+
+class Layout extends React.Component {
+  render() {
+    const { title, children } = this.props
+
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(27),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <header>
+          <h1
+            style={{
+              ...scale(1.5),
+              marginBottom: rhythm(1.5),
+              marginTop: 0,
+              textAlign: 'center',
+              fontFamily: '"Abril Fatface", serif',
+              fontWeight: 700,
+            }}
+          >
+            <Link
+              style={{
+                boxShadow: `none`,
+                textDecoration: `none`,
+                color: `inherit`,
+              }}
+              to={`/`}
+            >
+              {title}
+            </Link>
+          </h1>
+        </header>
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Layout
